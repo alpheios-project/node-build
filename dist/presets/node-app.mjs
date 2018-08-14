bbd3ebe0aa5804ccabf547d5649077f50b5cc7dd
Irina Sklyarova <irina060981@yandex.ru>
2018-08-14 21:28:00 +1000

    fix

--- dist/presets/node-app.mjs
+++ dist/presets/node-app.mjs
@@ -2,21 +2,23 @@ import path from 'path'
 const projectRoot = process.cwd()
 
 const webpack = {
-  target: "node",
-  entry: {
-    app: ["./src/index.js"]
-  },
-  output: {
-    path: path.resolve(projectRoot, "./dist"),
-    filename: "alpheios-lt-cmdtool.js"
-  },
-  externals: [],
-  mode: "development",
-  node: {
-    __dirname: false
-  },
-  resolve: {
-    modules: ['node_modules']
+  common: {
+    target: "node",
+    entry: {
+      app: ["./src/index.js"]
+    },
+    output: {
+      path: path.resolve(projectRoot, "./dist"),
+      filename: "alpheios-lt-cmdtool.js"
+    },
+    externals: [],
+    mode: "development",
+    node: {
+      __dirname: false
+    },
+    resolve: {
+      modules: ['node_modules']
+    }
   }
 }
 
